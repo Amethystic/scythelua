141153962ca08eef9b2d55efeb6611c35b5c7f99
Athena <119145022+Amethystic@users.noreply.github.com>
2023-08-09 03:48:35 -0500

    Update RayfieldCustom.luaddd

--- RayfieldCustom.lua
+++ RayfieldCustom.lua
@@ -46,7 +46,7 @@ local RayfieldLibrary = {
 
 			SliderBackground = Color3.fromRGB(102, 76, 106),
 			SliderProgress = Color3.fromRGB(185, 155, 190),
-			SliderStroke = Color3.fromRGB(6, 6, 6),
+			SliderStroke = Color3.fromRGB(0, 0, 0),
 
 			ToggleBackground = Color3.fromRGB(30, 30, 30),
 			ToggleEnabled = Color3.fromRGB(206, 147, 216),
