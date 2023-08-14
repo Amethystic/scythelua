2094996c750e5a58ee03ca65afa6ad75169f23e5
Athena <119145022+Amethystic@users.noreply.github.com>
2023-08-14 16:20:38 -0500

    Skywars

--- Gamefinder.lua
+++ Gamefinder.lua
@@ -15,7 +15,8 @@ Games = {
     [3956818381] = "https://raw.githubusercontent.com/TrixAde/Proxima-Hub/main/Main.lua", -- ninjago
     [142823291] = "https://raw.githubusercontent.com/OneProtocol/Project/main/Loader", -- MM2
     [9559194006] = "https://pastebin.com/raw/5LtLLSFE", -- SE
-    [6872265039] = "https://pastebin.com/raw/wSWEMgRW" -- BW (Test)
+    [6872265039] = "https://pastebin.com/raw/wSWEMgRW", -- BW
+    [8951451142] = "https://pastebin.com/raw/zNtWwkcu" -- SW
 }
 
 if Games[game.PlaceId] then
